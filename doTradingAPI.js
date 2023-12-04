05bfd295fa59450ef9cb2d0c23a4b90c630ae139
anismuhaimin <anis.muhaimin@sedaniainnovator.com>
2023-12-04 16:17:43 +0800

    Initial Commit

---
+++ doTradingAPI.js
@@ -0,0 +1,60 @@
+// app.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const doTradingSchema = require('./doTradingSchema');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Endpoint to create a new applicant
+app.post('/applicants', async (req, res) => {
+  try {
+    const {
+      ref_no,
+      cust_name,
+      financing_amount,
+      tenure,
+      mobile_no,
+    } = req.body;
+
+    const profit_rate = 4.9;
+
+    const profit_amount = financing_amount * (profit_rate / 100);
+    const selling_price = financing_amount + profit_amount;
+
+    const newApplicant = {
+      ref_no,
+      cust_name,
+      financing_amount,
+      tenure,
+      profit_rate,
+      profit_amount,
+      selling_price,
+      mobile_no,
+      product_id: 'D4',
+      operation_remarks: '-',
+    };
+
+    const applicantId = await doTradingSchema.createApplicant(newApplicant);
+
+    // Send a success response
+    res.status(201).json({ message: 'Applicant created successfully', id: applicantId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Endpoint to get all applicants
+app.get('/applicants', (req, res) => {
+  // Implement code to retrieve applicants from MySQL database
+  res.status(200).json({ message: 'Not implemented for MySQL' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ doTradingSchema.js
@@ -0,0 +1,38 @@
+// doTradingSchema.js
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test_node',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+const promisePool = pool.promise();
+
+const doTradingSchema = {
+  createApplicant: async (applicant) => {
+    const [rows] = await promisePool.execute(
+      'INSERT INTO applicants (ref_no, cust_name, financing_amount, tenure, profit_rate, profit_amount, selling_price, mobile_no, product_id, operation_remarks) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [
+        applicant.ref_no,
+        applicant.cust_name,
+        applicant.financing_amount,
+        applicant.tenure,
+        applicant.profit_rate,
+        applicant.profit_amount,
+        applicant.selling_price,
+        applicant.mobile_no,
+        applicant.product_id,
+        applicant.operation_remarks
+      ]
+    );
+    return rows.insertId;
+  }
+};
+
+module.exports = doTradingSchema;
